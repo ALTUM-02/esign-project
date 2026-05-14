30b2760c4ef925c10ed533b551b7c093c4f13c7d
ALTUM-02 <martin249jesus@gmail.com>
2026-05-14 10:56:55 +0300

    new change

--- esign-backend/api/urls.py
+++ esign-backend/api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
+from .views import RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from .views import TestView
 
 urlpatterns = [
     path("test/", TestView.as_view()),
+    path("register/", RegisterView.as_view()),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
