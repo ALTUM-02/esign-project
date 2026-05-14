852545697d7768e92b76a81cbb1109ec61752d56
ALTUM-02 <martin249jesus@gmail.com>
2026-05-14 03:39:04 +0300

    new change

---
+++ esign-backend/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+class TestView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "message": "Authenticated",
+            "user": request.user.username
+        })
