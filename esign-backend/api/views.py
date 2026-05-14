6a9b74ce41cd83176455937d1a672fb04f15ac9d
ALTUM-02 <martin249jesus@gmail.com>
2026-05-14 10:53:38 +0300

    new change

--- esign-backend/api/views.py
+++ esign-backend/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
 from rest_framework.permissions import AllowAny
-
-
 class TestView(APIView):
 
     permission_classes = [AllowAny]
@@ -11,3 +11,27 @@ class TestView(APIView):
         return Response({
             "message": "Backend working successfully"
         })
+        
+class RegisterView(APIView):
+
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+
+        serializer = RegisterSerializer(
+            data=request.data
+        )
+
+        if serializer.is_valid():
+
+            user = serializer.save()
+            
+
+            return Response({
+                "message": "User registered successfully"
+            }, status=status.HTTP_201_CREATED)
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )        
