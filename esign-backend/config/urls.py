73d11915c7984978388b64c0188d3a8096d9e4ba
ALTUM-02 <martin249jesus@gmail.com>
2026-05-17 08:00:10 +0300

    new change

--- esign-backend/config/urls.py
+++ esign-backend/config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('auth/', include('authentication.urls')),
+    path('api/auth/', include('authentication.urls')),
     path('documents/', include('documents.urls')),
 ]
 
