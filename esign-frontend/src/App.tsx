f5c0bdf11c1eb34f773c00d865a666c459ad7e6e
ALTUM-02 <martin249jesus@gmail.com>
2026-05-18 11:45:11 +0300

    new change

--- esign-frontend/src/App.tsx
+++ esign-frontend/src/App.tsx
@@ -2,4 +2,3 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
-import MainLayout from "./layouts/MainLayout
