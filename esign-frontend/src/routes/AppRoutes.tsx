fbce814dbea27812d5d4eba8e64395848913933e
ALTUM-02 <martin249jesus@gmail.com>
2026-05-18 11:26:51 +0300

    new change

--- esign-frontend/src/routes/AppRoutes.tsx
+++ esign-frontend/src/routes/AppRoutes.tsx
@@ -3,4 +3,3 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
-import 
