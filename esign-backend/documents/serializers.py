35c0a3fd7a76083757fd158052077a3345952859
ALTUM-02 <martin249jesus@gmail.com>
2026-05-26 08:31:25 +0300

    new change

---
+++ esign-backend/documents/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from .models import Document
+
+
+class DocumentSerializer(
+    serializers.ModelSerializer
+):
+
+    class Meta:
+
+        model = Document
+
+        fields = [
+            "id",
+            "title",
+            "file",
+            "uploaded_at",
+            "signed_file",
+            "signed",
+        ]
+
+        read_only_fields = [
+            "id",
+            "uploaded_at",
+        ]
