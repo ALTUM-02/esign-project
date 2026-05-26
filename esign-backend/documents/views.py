f2a6924b9e8988d5dc27aa5f585c941f5ed74195
ALTUM-02 <martin249jesus@gmail.com>
2026-05-26 08:35:21 +0300

    new change

--- esign-backend/documents/views.py
+++ esign-backend/documents/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.response import Response
-from rest_framework.decorators import (
-    api_view,
-    permission_classes,
-)
-
 from rest_framework.permissions import (
     IsAuthenticated,
 )
