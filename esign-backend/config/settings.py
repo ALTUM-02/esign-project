e0595d4246e29061c0397ace995280ce23ae00a9
ALTUM-02 <martin249jesus@gmail.com>
2026-06-09 02:20:25 +0300

    new change

--- esign-backend/config/settings.py
+++ esign-backend/config/settings.py
@@ -167,5 +167,5 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-replace-me')  
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-replace-m
 
